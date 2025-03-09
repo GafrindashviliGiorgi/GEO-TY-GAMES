0f0c018275df630b3bf89dd1991cda57f0f5394d
Giorgi Gafrindashvili <giorgigafrindashvili190@gmail.com>
2025-03-09 08:52:37 -0700

    ewv
    vwe

---
+++ zipper.js
@@ -0,0 +1,104 @@
+setInterval(() => {
+
+    let randomPeople = Math.round(Math.random()*10) +1
+    let bigDiv = document.querySelector(".bifDiv")
+    console.log(randomPeople);
+    
+    if(randomPeople == 1) {
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    }
+    if(randomPeople == 2) {
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    
+    }if(randomPeople == 3) {
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    
+    }if(randomPeople == 4) {
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    
+    }if(randomPeople == 5) {
+    
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    }if(randomPeople == 6) {
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    
+    }if(randomPeople == 7) {
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    
+    }if(randomPeople == 8) {
+    
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    }if(randomPeople == 9) {
+    
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+
+    </div>
+    </div>`
+    }if(randomPeople == 10) {
+    
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+    </div>
+    </div>`
+    }if(randomPeople == 11) {
+        bigDiv.innerHTML += `<div class="constumers">
+    <div  class="k1">
+        <p>გიორგი</p>
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; position: relative; bottom: 23px;">buzz cut</h1>
+    </div>
+    </div>`
+    }
+}, 2000);
