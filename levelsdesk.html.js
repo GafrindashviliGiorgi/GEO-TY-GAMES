a902b63286ae6eddc704295a9d1c2389a9c4aa7b
Giorgi Gafrindashvili <giorgigafrindashvili190@gmail.com>
2025-03-10 08:23:41 -0700

    sd
    sd

--- levelsdesk.html.js
+++ levelsdesk.html.js
@@ -5,7 +5,7 @@ let x = [
     {
         image:"h1ta.jpg",
         h3:"თამაში H1taზე",
-        p:"თამაში სადაც უნდა გამოიყენო შენი IQ",
+        p:"თამაში სადაც უნდა გამოიყენო შენი IQ, გქქვს მაღალი IQ",
     }, {
         image:"HungruMan.jpg",
         h3:"თამაშიHungruManებზე ",
@@ -26,16 +26,20 @@ let x = [
     {
         image:"Zipper.jpg",
         h3:"თამაში Zipperზე",
-        p:"თამაში სადაც უნდა აიღო რაც შეიძლება მეტი ზიპერი და დააბდდეითო საპარიკმახელოთი ნაშოვნი ფულით"
+        p:"თამაში სადაც უნდა შეჭრა ხალხს თმები და დაამყარო რეკორდი",
+        link:"zipper.html"
     }
 ]
 
 x.forEach((item) => {
-    big.innerHTML += `        <div class="k1">
+    big.innerHTML += `        <div class="k1" onclick="ku()">
             <img src="${item.image}" alt="">
             <div style="position: relative; top: 10px; font-size: 10px; text-align: center;">
                 <h3>${item.h3}</h3>
                 <p>${item.p}</p>
+
             </div>
+            <button class="ku"><a href=${item.link}>გადასვლა</a></button>
         </div>`
+
 })
